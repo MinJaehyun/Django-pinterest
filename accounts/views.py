ff8c220f325905ad2a2bfcf1c365285d34f9340b
{krism01} <{krism01@naver.com}>
2021-07-26 01:36:26 +0900

    client 에서 server 에 data 요청 시, DB에 저장하고 가져오는 방법

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from accounts.models import HelloWorld
 
 
 def hello_world(request):
     # post
     if request.method == "POST":
-        return render(request, 'accounts/hello_world.html', context={"text": "POST METHOD"})
+        temp = request.POST.get('hello_input')
+        hello_world = HelloWorld()
+        hello_world.text = temp
+        hello_world.save()
+
+        return render(request, 'accounts/hello_world.html', context={"hello_world_output": hello_world})
 
     # get
     else:
-        return render(request, 'accounts/hello_world.html', context={"text": "GET METHOD"})
+        return render(request, 'accounts/hello_world.html', context={"hello_world_output": "GET METHOD"})
