cd601576e452f6da73368b6ee8c897472536a15f
{krism01} <{krism01@naver.com}>
2021-07-28 17:24:27 +0900

    상세페이지로 이동하기 위한 get_success_url 설정 fix edit page

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,4 +1,4 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -12,13 +12,16 @@ class ProfileCreateView(CreateView):
     form_class = ProfileCreateForm
     context_object_name = 'target_profile'
     template_name = 'profileapp/create.html'
-    success_url = reverse_lazy('accounts:hello_world')
+    # success_url = reverse_lazy('accounts:detail')
 
     def form_valid(self, form):
         temp_profile = form.save(commit=False)
         temp_profile.user = self.request.user
         temp_profile.save()
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('accounts:detail', kwargs={'pk': self.object.user.pk})
 
 
 @method_decorator(profiles_ownership_required, 'get')
@@ -28,4 +31,7 @@ class ProfileUpdateView(UpdateView):
     form_class = ProfileCreateForm
     context_object_name = 'target_profile'
     template_name = 'profileapp/update.html'
-    success_url = reverse_lazy('accounts:hello_world')
+    # success_url = reverse_lazy('accounts:hello_world')
+
+    def get_success_url(self):
+        return reverse('accounts:detail', kwargs={'pk': self.object.user.pk})
