94317f6cc777362e01805efd3fe57e749c8f2b12
{krism01} <{krism01@naver.com}>
2021-07-28 15:22:57 +0900

    UpdateView 구현 및 decorator 적용 profile image setting ProfileUpdateView 에만 decorator 적용

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,6 +1,8 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profiles_ownership_required
 from profileapp.forms import ProfileCreateForm
 from profileapp.models import Profile
 
@@ -17,3 +19,13 @@ class ProfileCreateView(CreateView):
         temp_profile.user = self.request.user
         temp_profile.save()
         return super().form_valid(form)
+
+
+@method_decorator(profiles_ownership_required, 'get')
+@method_decorator(profiles_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    form_class = ProfileCreateForm
+    context_object_name = 'target_profile'
+    template_name = 'profileapp/update.html'
+    success_url = reverse_lazy('accounts:hello_world')
