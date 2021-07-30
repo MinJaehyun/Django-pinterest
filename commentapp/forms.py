ea43c12bf44b95215a8f3408a1f3d756b496f56b
{krism01} <{krism01@naver.com}>
2021-07-30 16:53:32 +0900

    complete commentapp 게시글 하단에 댓글을 입력하면 출력하는 기능 구현 본인이 아니면 댓글을 삭제할 수 없다 댓글은 현재 update 할 수 없도록 만들었다

--- commentapp/forms.py
+++ commentapp/forms.py
@@ -1,9 +1,9 @@
 from django.forms import ModelForm
 
-from articleapp.models import Article
+from commentapp.models import Comment
 
 
 class CommentCreationForm(ModelForm):
     class Meta:
-        model = Article
+        model = Comment       # FIXME: Article 에서 Comment 로 수정
         fields = ['content']
--- commentapp/views.py
+++ commentapp/views.py
@@ -1,7 +1,9 @@
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DeleteView
 
 from articleapp.models import Article
+from commentapp.decorators import comments_ownership_required
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
 
@@ -22,6 +24,14 @@ class CommentCreateView(CreateView):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
 
 
-# class CommentDeleteView(DeleteView):
+@method_decorator(comments_ownership_required, 'get')
+@method_decorator(comments_ownership_required, 'post')
+class CommentDeleteView(DeleteView):
     # m, f, c, t, s
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
 
